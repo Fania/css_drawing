68ec8c16cfee54388f79c271a9c2f1ec9b296304
Fania Everitt <me@fania.eu>
2025-04-04 19:42:26 +0100

    add js anim lists

--- kandinsky/scripts.js
+++ kandinsky/scripts.js
@@ -10,9 +10,11 @@ const zs = document.querySelectorAll('z');
 // console.log(xs);
 // console.log(zs);
 
-// u 11, 12, 21, 22, 25, 26, 27, 47, 54, 63
-// x 9
-const anims = [us[10],us[11],us[20],us[21],us[24],us[25],us[26],us[46],us[53],us[62],xs[8]];
+
+
+// u 11, 12, 21, 22, 25, 26, 27, 28, 29, 30, 31, 32, 33, 47, 54, 63
+// x 9, 10
+const anims = [us[10],us[11],us[20],us[21],us[24],us[25],us[26],us[27],us[28],us[29],us[30],us[31],us[32],us[46],us[53],us[62],xs[8],xs[9]];
 // console.log(anims);
 
 const rower = [us[10],us[11],us[20],us[21],us[23]];
